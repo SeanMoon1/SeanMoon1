57b8a021351edfc7ac2671e3d777733c05aa769e
SeungYeon Moon ( Sean ) <moonaki43@gmail.com>
2025-06-23 12:52:24 +0900

    Morning Study

---
+++ projects/study/0623/lecture/main.js
@@ -0,0 +1,26 @@
+ 
+/*alert("Hello, World!");
+
+console.log("Hello, World!");
+
+document.write("<h1>Document Write</h1>");
+
+let name = prompt("당신의 이름은?");
+alert("안녕하세요, "+ name + "님!");*/
+
+/*  let: 재할당 가능(변수)
+    const: 재할당 불가 (상수)
+    var: 호이스팅 문제로 사용 지양 */
+
+/*let currentYear = 2025;
+let birthYear = prompt("태어난 해를 입력하세요");
+let age = currentYear - birthYear + 1;
+alert("당신의 나이는 " + age + "세입니다.");*/
+
+let name = "철수";
+let age = 20;
+let isStudent = true;
+
+console.log(typeof name); // string
+console.log(typeof age); // number
+console.log(typeof isStudent); // boolean
