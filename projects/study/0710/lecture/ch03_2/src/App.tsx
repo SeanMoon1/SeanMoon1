f53cb08c86700dbbbfe1f67f1a7966c9ad784e4a
SeungYeon Moon ( Sean ) <moonaki43@gmail.com>
2025-07-10 14:25:21 +0900

    TailwindCss study

---
+++ projects/study/0710/lecture/ch03_2/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Tailwindcss from './pages/Tailwindcss'
+import Color from './pages/Color'
+import TextsTest from './pages/TextTest'
+
+export default function App() {
+  return (
+    <div>
+      <TextsTest />
+      <Color />
+      <Tailwindcss />
+    </div>
+  )
+}
