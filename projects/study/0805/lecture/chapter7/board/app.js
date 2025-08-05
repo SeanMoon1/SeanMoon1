0b206b80c06567f5ed0ac8aab7a68f1f45c2330a
SeungYeon Moon ( Sean ) <moonaki43@gmail.com>
2025-08-05 17:33:20 +0900

    Fix Express.js view engine configuration and remove test-yarn submodule

--- projects/study/0805/lecture/chapter7/board/app.js
+++ projects/study/0805/lecture/chapter7/board/app.js
@@ -1,13 +1,22 @@
 const express = require("express");
 const handlebars = require("express-handlebars");
+const path = require("path");
 const app = express();
 
-app.engine("handlebars", handlebars.engine());
+// Handlebars 엔진 설정
+app.engine("handlebars", handlebars.engine({
+    defaultLayout: "main",
+    layoutsDir: path.join(__dirname, "views/layouts"),
+    partialsDir: path.join(__dirname, "views/partials")
+}));
+
 app.set("view engine", "handlebars");
-app.set("views", __dirname + "/views");
+app.set("views", path.join(__dirname, "views"));
 
 app.get("/", (req, res) => {
-    res.render("home", { title: "테스트 게시판" })
-})
+    res.render("home", { title: "테스트 게시판" });
+});
 
-app.listen(3000)
+app.listen(3000, () => {
+    console.log("서버가 포트 3000에서 실행 중입니다.");
+});
